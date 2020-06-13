a4cd76f520f77fbfde7c39896f6712bf5bf5f9ca
Justin Lee <macintoshhd@MacBook-Pro.local>
2020-06-13 17:35:21 -0400

    added firebase util codes and google authentication buton

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyCvzSys3MYU6aHilO9RMMVeUmf6o0EmsRw",
+    authDomain: "crwn-db-6b1c9.firebaseapp.com",
+    databaseURL: "https://crwn-db-6b1c9.firebaseio.com",
+    projectId: "crwn-db-6b1c9",
+    storageBucket: "crwn-db-6b1c9.appspot.com",
+    messagingSenderId: "448136348650",
+    appId: "1:448136348650:web:cc1e208123cf45257f6893",
+    measurementId: "G-SYEBV25EVJ"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider)
+
+export default firebase;
