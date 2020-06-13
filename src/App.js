b8cb8e5ba1e65427ffc296e31fcf7534314e830a
Justin Lee <macintoshhd@MacBook-Pro.local>
2020-06-13 16:51:27 -0400

    added in sign-in-sign-up page and sign-in components and styalized buttons

--- src/App.js
+++ src/App.js
@@ -8,14 +8,18 @@ import ShopPage from './pages/shop/shops.component';
 
 import Header from './components/header/header.component';
 
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
+
 
 function App() {
   return (
     <div>
     <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route path='/shop' component={ShopPage} />
+      <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
